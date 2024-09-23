dee0957d7d0a74be29c63e6d8cf98050f6d33cba
emarketguy27 <j.dennisuk@gmail.com>
2024-09-23 09:13:40 +0200

    Updated scripts and html structure

--- animations.js
+++ animations.js
@@ -1,4 +1,4 @@
-startLoader();
+
 
 // Lenis smooth-scroll update gsap for viewport position tracking
 const lenis = new Lenis();
@@ -10,52 +10,24 @@ function raf(time) {
   requestAnimationFrame(raf);
 }
 
-// Site Pre-Loader Scripts
-function startLoader() {
-  let counterElement = document.querySelector(".count");
-  let currentValue = 0;
-
-  // const letters = new SplitType("ml16", { text: "chars" });
-  function updateCounter() {
-    if (currentValue < 100) {
-      let increment = Math.floor(Math.random() * 10) + 1;
-      currentValue = Math.min(currentValue + increment, 100);
-      counterElement.textContent = currentValue;
-
-      let delay = Math.floor(Math.random() * 200) + 25;
-      setTimeout(updateCounter, delay);
-    }
-  }
-  updateCounter();
-}
-
 requestAnimationFrame(raf); // The Counter Animation
 
-gsap.to(".count", {
-  opacity: 0,
-  delay: 3.5,
-  duration: 0.5,
-});
-
-let textWrapper = document.querySelector(".ml16");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\s/g,
-  "<span class='letter'>$&</span>"
-);
-
 // Split Text for animating title
-const letters = new SplitType(".ml16", {
-  text: "chars",
-});
+  // const letters = new SplitType(".ml16", {
+  //   text: "chars",
+  // });
 
-const links = new SplitType(".links", {
-  text: "words",
-});
+  const links = new SplitType(".links", {
+    text: "words",
+  });
 
 // Navigation Links
-gsap.to(".links", {
+gsap.fromTo(".links", {
+  y: -10,
+}, {
+  y: 0,
   opacity: 1,
-  delay: 5,
+  delay: 8.5,
 });
 
 gsap.fromTo(links.words,
@@ -66,83 +38,87 @@ gsap.fromTo(links.words,
   {
     y: 0,
     opacity: 1,
-    delay: 6,
+    delay: 8.5,
     stagger: 0.05,
   }
 );
 
 // Entry Logo text animate to nav bar
-gsap.to(".copy", {
-  x: -760,
-  y: -430,
-  opacity: 0,
-  delay: 5,
-  duration: 2,
-  ease: "power4.out",
-});
+  // gsap.to(".copy", {
+  //   x: -760,
+  //   y: -430,
+  //   opacity: 0,
+  //   delay: 5,
+  //   duration: 2,
+  //   ease: "power4.out",
+  // });
 
 gsap.to(".logo", {
   opacity: 1,
   duration: 0.4,
-  delay: 6,
+  delay: 8.5,
 });
 
 // The Title animation
-gsap.to(".ml16", {
-  opacity: 1,
-});
+  // gsap.to(".ml16", {
+  //   opacity: 1,
+  // });
 
-gsap.from(letters.chars, {
-  opacity: 0,
-  y: -10,
-  ease: "bounce.out",
-  duration: 1.5,
-  delay: 0.05,
-  stagger: 0.05,
-  repeat: 1,
-});
+  // gsap.from(letters.chars, {
+  //   opacity: 0,
+  //   y: -10,
+  //   ease: "bounce.out",
+  //   duration: 1.5,
+  //   delay: 0.05,
+  //   stagger: 0.05,
+  //   repeat: 1,
+  // });
 
-// Animate Black Background down
+// ********** entrance to main content animation sequence
+// 1 animations container
 gsap.to(".pre-loader", {
   scale: 0.5,
   ease: "power4.inOut",
   duration: 2,
-  delay: 3,
+  delay: 6,
 });
-
-// Animate Black Box to brand
+// 2 Animate screen from gray Box to brand
 gsap.to(".loader", {
   height: "0",
   ease: "power4.inOut",
   duration: 1.5,
-  delay: 3.75,
+  // delay: 3.75,
+  delay: 7,
 });
-
-// Animate brand-color box out to top
-gsap.to(".loader-bg", {
-  height: "0",
-  ease: "power4.inOut",
-  duration: 1.5,
-  delay: 4.5,
-});
-
-// Animate In Main White Background and content
+//  3 Animate slide up-In Main dark Background
 gsap.to(".loader-2", {
   clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
   ease: "power4.inOut",
   duration: 1.5,
-  delay: 3.5,
+  delay: 7.5,
+});
+// 4 Animate brand-color box out to top
+gsap.to(".loader-bg", {
+  height: "0",
+  ease: "power4.inOut",
+  duration: 1.5,
+  // delay: 4.5,
+  delay: 7.2,
 });
 
-// Fade-out header letters
+
+
+// Fade-in header letters
 gsap.to(".pl-header h1", {
   opacity: 1,
   y: 0,
   ease: "power4.inOut",
   duration: 1.5,
-  delay: 4.25,
-  stagger: 0.05,
+  // delay: 4.25,
+  delay: 7.25,
+  stagger: 0.08,
 });
+
 // Fade-in Header-mirror letters
 gsap.fromTo(".pl-header-mirror h1", {
   y: 100, },
@@ -150,11 +126,12 @@ gsap.fromTo(".pl-header-mirror h1", {
   y: 0,
   ease: "power4.inOut",
   duration: 1.5,
-  delay: 4.35,
-  stagger: 0.05,
+  // delay: 4.35,
+  delay: 7.35,
+  stagger: 0.08,
 });
 
-// Animate H1 main heading on scroll with Tieline
+// Animate H1 main heading on scroll with Timeline
 var H1Tl = gsap.timeline({
   defaults: {
     duration: 3,
@@ -172,7 +149,8 @@ H1Tl.to (".pl-header h1", {
   y: -200,
   duration: 3,
 });
-// Mirror
+
+// // Mirror
 var H1mTl = gsap.timeline({
   defaults: {
     duration: 3,
@@ -194,23 +172,23 @@ H1mTl.to(".pl-header-mirror h1", {
 
 
 // Footer Bio Animate in
-gsap.to(".footer-copy p", {
-  opacity: 1,
-  y: 0,
-  delay: 5.5,
-  duration: 3,
-  ease: "elastic.out",
-  stagger: 0.25,
-});
+// gsap.to(".footer-copy p", {
+//   opacity: 1,
+//   y: 0,
+//   delay: 5.5,
+//   duration: 3,
+//   ease: "elastic.out",
+//   stagger: 0.25,
+// });
 
 // Animate Footer Images in
-gsap.to(".img", {
-  clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-  ease: "power4.inOut",
-  duration: 1.5,
-  delay: 4.5,
-  stagger: 0.25,
-});
+// gsap.to(".img", {
+//   clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+//   ease: "power4.inOut",
+//   duration: 1.5,
+//   delay: 4.5,
+//   stagger: 0.25,
+// });
 
   // Scrolling Marquee Texts
 const scrollTl = gsap.timeline({
@@ -258,6 +236,7 @@ page2Tl.from(".services", {
   y: -200,
 });
 const services = gsap.utils.toArray(".service"); // set 'service' sections into an array
+
 // set up intersection observer parameters
 const observerOptions = {
   root: null,
